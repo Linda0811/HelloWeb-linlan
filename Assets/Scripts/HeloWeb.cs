4368c0c6415618f01f491ceeff90bccaf8208ae3
Linda0811 <langegger.linda@icloud.com>
2019-02-20 16:05:27 +0100

    add unity

---
+++ Assets/Scripts/HeloWeb.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeloWeb : MonoBehaviour
+{
+    string helloweb = "HelloWeb";
+
+    public void PrintHelloWeb()
+    {
+        Debug.Log(helloweb);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
